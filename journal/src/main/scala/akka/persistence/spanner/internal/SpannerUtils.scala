1482b83cd89f093a2f8cb1059c2c7a185f3e94bc
Johan Andrén <johan@markatta.com>
2020-04-23 09:24:53 +0200

    Spanner snapshot store #41

---
+++ journal/src/main/scala/akka/persistence/spanner/internal/SpannerUtils.scala
@@ -0,0 +1,19 @@
+package akka.persistence.spanner.internal
+
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalField
+
+import akka.annotation.InternalApi
+
+/**
+ * INTERNAL API
+ */
+@InternalApi
+private[spanner] object SpannerUtils {
+  def unixTimestampMillisToSpanner(unixTimestampMs: Long): String =
+    Instant.ofEpochMilli(unixTimestampMs).toString // I _think_ this will always be rfc3339
+
+  def spannerTimestampToUnixMillis(spannerTimestamp: String): Long =
+    Instant.parse(spannerTimestamp).toEpochMilli
+}
