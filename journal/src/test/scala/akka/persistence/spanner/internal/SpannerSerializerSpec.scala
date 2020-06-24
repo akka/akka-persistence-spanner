aebf939c2bc9d7800daf524c08de0ca571affa3c
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-06-24 14:18:50 +0200

    use unixTimestampMillisToSpanner in test

--- journal/src/test/scala/akka/persistence/spanner/internal/SpannerSerializerSpec.scala
+++ journal/src/test/scala/akka/persistence/spanner/internal/SpannerSerializerSpec.scala
@@ -3,8 +3,6 @@
  */
 package akka.persistence.spanner.internal
 
-import java.sql.Timestamp
-
 import akka.actor.ExtendedActorSystem
 import akka.actor.testkit.typed.scaladsl.LogCapturing
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
@@ -14,14 +12,13 @@ import org.scalatest.wordspec.AnyWordSpecLike
 
 class SpannerSerializerSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike with LogCapturing {
   private val serializer = new SpannerSerializer(system.classicSystem.asInstanceOf[ExtendedActorSystem])
-  private val commitTimestamp = Timestamp.valueOf("2020-06-24 10:35:15.123456").toString
+  private val commitTimestamp = SpannerUtils.unixTimestampMillisToSpanner(System.currentTimeMillis())
 
   "SpannerSerializer" must {
     Seq(
       "SpannerOffset-1" -> SpannerOffset(commitTimestamp, Map.empty),
       "SpannerOffset-2" -> SpannerOffset(commitTimestamp, Map("pid1" -> 5L)),
-      "SpannerOffset-3" -> SpannerOffset(commitTimestamp, Map("pid1" -> 5L, "pid2" -> 3L, "pid3" -> 7L))
-    ).foreach {
+      "SpannerOffset-3" -> SpannerOffset(commitTimestamp, Map("pid1" -> 5L, "pid2" -> 3L, "pid3" -> 7L))).foreach {
       case (scenario, item) =>
         s"resolve serializer for $scenario" in {
           SerializationExtension(system).findSerializerFor(item).getClass should be(classOf[SpannerSerializer])
