92ab949cf55879d2a186edc5a16059ec6921ee05
Scala Steward <me@scala-steward.org>
2021-06-10 18:04:22 +0200

    Update akka-actor-testkit-typed, ... to 2.6.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val Scala212 = "2.12.14"
   val Scala213 = "2.13.1"
 
-  val AkkaVersion = System.getProperty("override.akka.version", "2.6.9")
+  val AkkaVersion = System.getProperty("override.akka.version", "2.6.15")
   val AkkaVersionInDocs = AkkaVersion.take(3)
   // for example
   val AkkaHttpVersion = "10.2.3"
